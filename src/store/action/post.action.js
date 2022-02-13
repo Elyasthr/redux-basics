dd58402a74746024c0973815a264b162eb9860c5
Elyas <touahria.elyas@outlook.com>
2022-02-14 00:58:16 +0100

    redux crud

--- src/store/action/post.action.js
+++ src/store/action/post.action.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 
 export const GET_POST = "GET_POST";
+export const ADD_POST = "ADD_POST";
+export const EDIT_POST = "EDIT_POST";
+export const DELETE_POST = "DELETE_POST";
 
 export const getPost = () => {
   return (dispatch) => {
@@ -9,6 +12,44 @@ export const getPost = () => {
       .get('http://localhost:3000/posts?_sort=id&_order=desc')
       .then((res) => {
         dispatch({ type: GET_POST, payload: res.data })
+      })
+      .catch((err) => console.log(err))
+  }
+}
+
+export const addPost = (data) => {
+  return (dispatch) => {
+    return axios
+      .post('http://localhost:3000/posts', data)
+      .then((res) => {
+        dispatch({ type: ADD_POST, payload: data })
+      })
+      .catch((err) => console.log(err))
+  }
+}
+
+export const editPost = (data) => {
+  return (dispatch) => {
+    return axios({
+      method: "put",
+      url: `http://localhost:3000/posts/${data.id}`,
+      data: { ...data }
+    })
+      .then((res) => {
+        dispatch({ type: EDIT_POST, payload: { ...data } })
+      })
+      .catch((err) => console.log(err))
+  }
+}
+
+export const deletePost = (postId) => {
+  return (dispatch) => {
+    return axios({
+      method: "delete",
+      url: `http://localhost:3000/posts/${postId}`,
+    })
+      .then((res) => {
+        dispatch({ type: DELETE_POST, payload: { postId } })
       })
       .catch((err) => console.log(err))
   }
--- src/store/reducers/post.reducer.js
+++ src/store/reducers/post.reducer.js
@@ -1,4 +1,4 @@
-import { GET_POST } from "../action/post.action";
+import { GET_POST, ADD_POST, EDIT_POST, DELETE_POST } from "../action/post.action";
 
 const initialState = {}
 
@@ -7,6 +7,19 @@ export default function postReducer(state = initialState, action) {
   switch (action.type) {
     case GET_POST:
       return action.payload;
+    case ADD_POST:
+      return [action.payload, ...state];
+    case EDIT_POST:
+      return state.map((post) => {
+        if (post.id === action.payload.id) {
+          return {
+            ...post,
+            content: action.payload.content
+          }
+        } else return post
+      });
+    case DELETE_POST:
+      return state.filter((post) => post.id !== action.payload.postId);
     default:
       return state;
   }
